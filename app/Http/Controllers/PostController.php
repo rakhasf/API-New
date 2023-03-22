58c62b566d1d0f3e8ce050cfa092bc633c98eaa3
rakhasf <rakhasaptafabian@gmail.com>
2023-03-22 11:14:01 +0700

    Third Commitments

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\PostDetailResource;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -17,10 +18,51 @@ class PostController extends Controller
 
     public function show($id){
         $post = Post::with('writer:id,username')->findOrFail($id);
-        return new PostDetails($post);
+        return new PostDetailResource($post);
     }
-    public function show_too($id){
+    public function show2($id){
         $post = Post::findOrFail($id);
-        return new PostDetails($post);
+        return new PostDetailResource($post);
     }
+
+    public function store(Request $request){
+        $request -> validate([
+            'title' => 'required|max:255',
+            'article_content' => 'required',
+        ]);
+
+        // return response()->json('sudah dapat digunakan');
+        $request['author'] = Auth::user()->id;
+
+        $post = Post::create($request->all());
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
+
+        
+    }
+
+    public function update(Request $request, $id){
+    $request -> validate([
+        'title' => 'required|max:255',
+        'article_content' => 'required',
+    ]);
+
+    return response()->json('STATUS: USABLE');
+    return response()->json('sudah dapat digunakan');
+    $post = Post::findOrFail($id);
+    $post->update($request->all());
+
+    // return response()->json('sudah dapat digunakan');
+    return new PostDetailResource($post->loadMissing('writer:id,username'));
+
+    }
+
+    public function delete($id){
+        $post = Post::findOrFail($id);
+        $post->delete();
+
+        return response()->json([
+        'message' => "data successfully deleted"
+        ]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,12 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::middleware(['auth:sanctum'])->group(function (){
+    Route::get('/posts', [PostController::class, 'index']);
+    Route::get('/posts/{id}', [PostController::class, 'show']);
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::patch('/posts/{id}', [PostController::class, 'update'])->middleware(['post.owner']);
+    Route::delete('/posts/{id}', [PostController::class, 'delete'])->middleware(['post.owner']);
+    
+});
 Route::get('/posts2/{id}', [PostController::class, 'show_too']);
